95d2888f40ba5f3fdecacb3775a3dc904057d102
Nathanael Whitrow <nathanael.whitrow@gmail.com>
2021-04-18 12:42:33 +0930

    Minimal working c++ implementation

--- decorator_pattern/c++/main.cpp
+++ decorator_pattern/c++/main.cpp
@@ -1,25 +1,81 @@
 #include <iostream>
+#include <memory>
 #include <string>
 
 using namespace std;
 
-class beverage
+
+// Abstract base class
+class Beverage
 {
 public:
-    beverage() {}
-    ~beverage() {};
+    Beverage() {};
+    virtual ~Beverage() {};
 
-    string getDescription() { return drink; }
+    virtual float  getCost()        const = 0; // pure virtual makes this class abstract
+    virtual string getDescription() const = 0; // pure virtual makes this class abstract
 
-private:
-    string drink = "unknown beverage";
+protected:
+    string description;
+    float cost;
 };
+
+
+
+// Concrete implementation
+class Espresso : public Beverage
+{
+public:
+    Espresso() {
+       description = "espresso";
+       cost = 2.99;
+    };
+    ~Espresso() {};
+
+    float  getCost()        const override { return cost; }
+    string getDescription() const override { return description; }
+};
+
+
+// Decorator class
+class Decorator : public Beverage
+{
+public:
+    Decorator(Beverage* drink) : drink_(drink) {};
+    ~Decorator() {};
+
+    // delegate as much as possible to the wrapped components
+    float       getCost()        const override { return this->drink_->getCost(); }
+    std::string getDescription() const override { return this->drink_->getDescription(); }
+
+protected:
+    Beverage* drink_;
+};
+
+
+// Concrete decorator
+class Mocha : public Decorator
+{
+public:
+    Mocha(Beverage* beverage) : Decorator(beverage) {};
+    ~Mocha() override {};
+
+    float getCost()         const override { return 1.00 + Decorator::getCost(); };
+    string getDescription() const override { return "Mocha " + Decorator::getDescription(); };
+};
+
+
 
 int main()
 {
-    beverage first_drink;
-    cout << "hello world!" << endl;
-    cout  << first_drink.getDescription() << endl;
+    Beverage* first_drink = new Espresso();
+    cout  << first_drink->getDescription() << endl;
+    Beverage* second_drink = new Mocha(first_drink);
+    cout << second_drink->getDescription() << endl;
+
+
+    delete first_drink;
+    delete second_drink;
 
     return 0;
 }
