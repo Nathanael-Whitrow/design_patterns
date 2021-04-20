b46f9b7c2fe9ebbb02f02651f213881b6ae06b0a
Nathanael Whitrow <nathanael.whitrow@gmail.com>
2021-04-20 14:31:10 +0930

    Manage memory with smart pointers

--- decorator_pattern/c++/main.cpp
+++ decorator_pattern/c++/main.cpp
@@ -6,74 +6,85 @@ using namespace std;
 
 
 // Abstract base class
+// Don't store description in here to leave it as an interface
+// It can also be made pure virtual
 class Beverage
 {
 public:
-    Beverage() {};
-    virtual ~Beverage() {};
-
-    // pure virtual makes this class abstract
-    virtual float  getCost()        const = 0;
+    virtual ~Beverage() = default; // destructor
+    virtual double getCost()        const = 0; // =0; makes this pure virtual
     virtual string getDescription() const = 0;
-
-protected:
-    string description;
-    float cost;
 };
 
 
-
-// Concrete implementation
+// Concrete implementations
 class Espresso : public Beverage
 {
 public:
-    Espresso() {
-       description = "espresso";
-       cost = 2.99;
-    };
-    ~Espresso() {};
+    double getCost()        const override { return 2.99; }
+    string getDescription() const override { return "Espresso"; }
+};
 
-    float  getCost()        const override { return cost; }
-    string getDescription() const override { return description; }
+class HouseBlend : public Beverage
+{
+public:
+    double getCost()        const override { return 3.50; }
+    string getDescription() const override { return "House blend coffee"; }
 };
 
 
 // Decorator class
-class Decorator : public Beverage
+// Because the decorator class is so similar to beverage, it could be replaced with
+// using CondimentDecorator = beverage;
+// then have each of the decorator implementations hold a unique_ptr to a Beverage
+class CondimentDecorator : public Beverage
 {
 public:
-    Decorator(Beverage* drink) : drink_(drink) {};
-    ~Decorator() {};
+    CondimentDecorator(unique_ptr<Beverage> drink) : drink_{std::move(drink)} {};
 
-    // delegate as much as possible to the wrapped components
-    float       getCost()        const override { return this->drink_->getCost(); }
+    double      getCost()        const override { return this->drink_->getCost(); }
     std::string getDescription() const override { return this->drink_->getDescription(); }
 
 protected:
-    Beverage* drink_;
+    // Smart pointers mean no memory leaks, and, *apparently*, better representation of memory ownership
+    unique_ptr<Beverage> drink_;
 };
 
 
-// Concrete decorator
-class Mocha : public Decorator
+// Concrete decorators
+class Mocha : public CondimentDecorator
 {
 public:
-    Mocha(Beverage* beverage) : Decorator(beverage) {};
-    ~Mocha() override {};
-
-    float getCost()         const override { return 1.00 + Decorator::getCost(); };
-    string getDescription() const override { return "Mocha " + Decorator::getDescription(); };
+    Mocha(unique_ptr<Beverage> beverage) : CondimentDecorator{ move(beverage) } {};
+    double getCost()        const override { return 1.00 + CondimentDecorator::getCost(); };
+    string getDescription() const override { return "Mocha " + CondimentDecorator::getDescription(); };
 };
+
+
+class Whip : public CondimentDecorator
+{
+public:
+    Whip(unique_ptr<Beverage> beverage) : CondimentDecorator{ move(beverage) } {};
+    double getCost()        const override { return 0.50 + CondimentDecorator::getCost(); };
+    string getDescription() const override { return "Whip " + CondimentDecorator::getDescription(); };
+};
+
+
 
 
 
 int main()
 {
-    unique_ptr<Beverage> first_drink = make_unique<Espresso>();
-    cout << "ordering one " << first_drink->getDescription() << " for " << first_drink->getCost() << endl;
+    const Beverage& beverage1 = Espresso{};
+    cout << "ordering one " << beverage1.getDescription() << " for $" << beverage1.getCost() << endl;
 
-    unique_ptr<Beverage> second_drink = make_unique<Mocha>(first_drink.get());
-    cout << "Making it a " << second_drink->getDescription() << " for " << second_drink->getCost() << endl;
+    // Using smart pointers
+    unique_ptr<Beverage> beverage2 = make_unique<HouseBlend>();
+    cout << "ordering one " << beverage2->getDescription() << " for $" << beverage2->getCost() << endl;
+
+    // Decorating
+    beverage2 = make_unique<Mocha>( std::move(beverage2) );
+    cout << "ordering one " << beverage2->getDescription() << " for $" << beverage2->getCost() << endl;
 
     return 0;
 }
